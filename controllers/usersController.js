0664712d3c5c8918afe7e52b119f511831a4a2da
Ting-Chou Lin <lmr3796joseph@gmail.com>
2019-02-15 11:43:02 -0800

    Add solutions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,24 +1,41 @@
 const User = require("../models/user");
 class UserController {
-
 	static getAllUsers(req, res) {
-
+		User.find()
+		.then(users => {
+			res.status(200)
+			.send(users)
+		})
 	}
 
 	static getUserById(req, res) {
-
+		User.findOne({userName: req.params.userId})
+		.then(user => {
+			res.status(200)
+			.send(user)
+		});
 	}
 
 	static createUser(req, res) {
-
+		let user = new User(req.body)   // BAD practice
+		user.save()
+		.then(savedData => {
+			res.status(201).send(savedData)
+		})
 	}
 
 	static updateUser(req, res) {
-
+		User.update({userName: req.params.userId}, req.body)
+			.then(result => {
+				res.status(200).send(result)
+			})
 	}
 
 	static deleteUser(req, res) {
-
+		User.deleteOne({userName: req.params.userId})
+		.then(user => {
+			res.status(204).send('')
+		});
 	}
 }
 
